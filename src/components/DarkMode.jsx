9d639e8fd7c378ed433b4873a5f898f73452329b
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-28 21:48:40 +0200

    feat: dark-mode

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,27 @@
+//<FontAwesomeIcon icon="fa-light fa-moon" />
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+
+const DarkMode = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('selectedTheme') || 'light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+    localStorage.setItem('selectedTheme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <div className="dark-mode">
+      <button className="dark-mode-toggle" onClick={toggleTheme}>
+        <FontAwesomeIcon icon={theme === 'dark' ? faSun : faMoon} size="2x" />
+      </button>
+    </div>
+  );
+};
+
+export default DarkMode;
