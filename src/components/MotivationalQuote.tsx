f9a7b2198eee52159a52818272c3874d2af699b3
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-30 22:59:04 +0200

    feat: start fetching

--- src/components/MotivationalQuote.tsx
+++ src/components/MotivationalQuote.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from 'react';
+
 const MotivationalQuote = () => {
+  const [quote, setQuote] = useState<string>('');
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch('https://zenquotes.io/api/today', { signal, mode: 'no-cors' })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.length) {
+          setQuote(data[0]?.q);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.name !== 'AbortError') {
+          setError("Something went wrong getting today's quote");
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
   return (
     <div className="quote-container widget-container">
-      <p>“Motivating quote of the day goes here“</p>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <h2>Today's Motivational Quote</h2>
+      <p>{quote}</p>
     </div>
   );
 };
