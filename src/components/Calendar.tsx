e593eac2544e2a8b54bf0cb35783394cd7192e29
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-30 15:10:43 +0200

    feat: date-component (#7)

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,8 +1,35 @@
-const Calendar = () => {
+import { useState, useEffect } from 'react';
+
+interface DateObject {
+  date: number;
+  month: string;
+}
+
+function capitalizedMonth(string: string): string {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getDate(): DateObject {
+  const day = new Date();
+  let month = day.toLocaleString('default', { month: 'long' });
+  month = capitalizedMonth(month);
+  const date = day.getDate();
+  return { date, month };
+}
+
+const Calendar: React.FC = () => {
+  const [currentDate, setCurrentDate] = useState<DateObject>({ date: 0, month: '' });
+
+  useEffect(() => {
+    setCurrentDate(getDate());
+  }, []);
+
   return (
     <div className="calendar-container widget-container">
-      <h2>26</h2>
-      <p>June</p>
+      <div className="date-wrapper">
+        <p className="date">{currentDate.date}</p>
+        <p className="month">{currentDate.month}</p>
+      </div>
     </div>
   );
 };
